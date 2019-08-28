01b07c0c46683561fe17b92e1e67e14d51ef381b
Zachary Reiss <zachary.reiss123@gmail.com>
2019-08-28 10:06:00 -0600

    Before adding On Disk Data

---
+++ OdeToFood.Data/OnDiskRestaurantData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class OnDiskRestaurantData : IRestaurantData
+    {
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Restaurant Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Restaurant GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCountOfRestaurants()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -25,7 +25,15 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
+
+            services.AddDbContextpool<OdeToFoodDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("OdeToFoodDb"));
+            });
+            
+        
+
+            services.AddScoped<IRestaurantData, SqlRestaurantData>();
 
             services.Configure<CookiePolicyOptions>(options =>
             {
